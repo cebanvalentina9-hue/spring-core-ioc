ab57cb333d2c6e83074bae54dc09051a399c9e5d
Valentina Ceban <cebanvalentina9@gmail.com>
2026-08-29 10:16:20 +0300

    Make all beans lazy

--- src/main/java/pixel/academy/spring_core_ioc/rest/DemoController.java
+++ src/main/java/pixel/academy/spring_core_ioc/rest/DemoController.java
@@ -13,7 +13,7 @@ public class DemoController {
 
     //constructor
     @Autowired
-    public DemoController(@Qualifier("turkishChef")Chef theChef) {
+    public DemoController(@Qualifier("frenchChef")Chef theChef) {
         myChef = theChef;
     }
 
